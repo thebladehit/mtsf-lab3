7fd3bf7718b883c2b009fec864e1b9a56e9952b0
thebladehit <boqdanyarmolka@gmail.com>
2024-03-26 19:43:28 +0200

    Add new endpoint, requirements

--- python-cahpter/spaceship/routers/api.py
+++ python-cahpter/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,17 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("/multiply_matrices")
+async def multiply_matrices():
+    matrix_a = np.random.rand(10, 10)
+    matrix_b = np.random.rand(10, 10)
+    
+    product = np.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
