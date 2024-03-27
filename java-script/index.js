4bc5bb2689e6360ac5836d5e16fe06cf246cecb5
thebladehit <boqdanyarmolka@gmail.com>
2024-03-27 16:27:09 +0200

    Add java-script repository

---
+++ java-script/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('Hello there');
+});
+
+app.get('bye', (req, res) => {
+  res.end('bye');
+});
+
+app.listen(8080, () => {
+  console.log('Listening on http://0.0.0.0:8080');
+});
